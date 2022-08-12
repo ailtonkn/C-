cb7d38169e14f7a482a531f1b68e9f86d08c89a6
Ailton Soares <100887767+ailtonkn@users.noreply.github.com>
2022-08-11 22:13:44 -0300

    Add files via upload

---
+++ Aula2/projeto02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace projeto02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+            
+             Console.WriteLine("Informe a Largura:");
+             double largura = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Informe a altura:");
+             double altura = Convert.ToDouble(Console.ReadLine());
+             double area = CalculaArea(largura, altura);
+             Console.WriteLine("A area equivale a:");
+             Console.WriteLine(area);
+            }
+            catch(Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+           
+
+        }
+
+        static double CalculaArea(double largura, double altura)
+        {
+            double area = largura * altura;
+            return area;
+        }
+    }
+}
